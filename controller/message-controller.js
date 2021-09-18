0eb023f83c2369b22b8022fb29c514d81689ba71
Rahul <rahulkeshri2491@gmail.com>
2021-09-18 23:34:28 +0530

    final commit

--- controller/message-controller.js
+++ controller/message-controller.js
@@ -1,4 +1,5 @@
 import Message from "../model/message.js";
+import Conversation from '../model/conversation.js';
 
 
 
@@ -6,6 +7,7 @@ export const newMessage = async(req,res) => {
     const newMessage = new Message(req.body);
     try {
         await newMessage.save();
+        await Conversation.findByIdAndUpdate(req.body.conversationId,{message:req.body.text});
         res.status(200).send('message save successfully');
     } catch (error) {
         res.status(500).json(error);
